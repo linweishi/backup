7422cbc9bd3c53ed42f2236a7a965bd3f853dbbd
linweishi <linweishi@outlook.com>
2020-10-17 16:52:45 +0800

    完成左侧导航栏基本布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -9,6 +22,9 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 
 // 将Message弹框提示全局挂载到 Vue， $message 为自定义属性
 Vue.prototype.$message = Message
